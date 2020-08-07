e4761a75a0c9f6e79880793f0b66a9ecf269fda8
Huan Feng <huan.feng@microfocus.com>
2020-08-07 08:10:47 +0800

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {NgxQRCodeModule} from 'ngx-qrcode2';
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        HomePageRoutingModule,
+        NgxQRCodeModule
+    ],
   declarations: [HomePage]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {BarcodeScanner} from '@ionic-native/barcode-scanner/ngx';
+import {Base64ToGallery} from '@ionic-native/base64-to-gallery/ngx';
+import {ToastController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    qrData = 'https://ionicacademy.com/';
+    scannedCode = null;
+    elementType: 'url' | 'canvas' | 'img' = 'canvas';
+
+    constructor(private barcodeScanner: BarcodeScanner,
+                private base64ToGallery: Base64ToGallery,
+                private toastCtrl: ToastController) {
+    }
+
+    scanCode() {
+        this.barcodeScanner.scan().then(
+            barcodeData => {
+                this.scannedCode = barcodeData.text;
+            }
+        );
+    }
 
 }
